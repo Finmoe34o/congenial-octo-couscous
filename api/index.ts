68ab9fe448eae4e1a967462a635694a3661e91f1
Finmoe34o <finleymoore0803@gmail.com>
2025-04-03 16:41:02 +0100

    asd

--- api/index.ts
+++
@@ -1,8 +0,0 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  res.status(200).json({ 
-    status: 'online',
-    message: 'FreelancerAI Pricing API is running' 
-  });
-}
